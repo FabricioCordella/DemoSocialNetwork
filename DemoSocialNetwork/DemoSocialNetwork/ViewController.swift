c9646fcbf00d09f9884a1d03b033a6a516557d15
Fabricio <fabriciotcordella@gmail.com>
2020-07-25 23:25:57 -0300

    started commit with environment

---
+++ DemoSocialNetwork/DemoSocialNetwork/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoSocialNetwork
+//
+//  Created by Fabricio Cordella on 25/07/20.
+//  Copyright © 2020 Fabricio Cordella. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
